601b453816e8e19b2fe0cbae1c769cb6bb4fd8c9
domagoj simic <domagoj.simic@factory.hr>
2018-02-27 12:31:15 +0100

    qfix

--- src/Migrations/2018_02_27_141210_alter_topic_comments_table.php
+++ src/Migrations/2018_02_27_141210_alter_topic_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTopicCommentsTable extends Migration
+class AlterTopicCommentsTable extends Migration
 {
     /**
      * Run the migrations.
